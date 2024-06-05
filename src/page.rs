13018760d6b450b2c2a35bbe47216297449458fc
gcournelle <gabriel.cournelle@partnerre.com>
2024-06-05 11:13:43 +0200

    Add record parsing but problems with overflow

--- src/page.rs
+++ src/page.rs
@@ -21,6 +21,7 @@ pub struct PageHeader {
     pub right_most_pointer: u32,
 }
 
+/// The page type is coded on a unique byte
 #[derive(Debug, PartialEq)]
 #[binrw]
 pub enum PageType {
@@ -61,6 +62,7 @@ pub struct BTreeTableInteriorCell {
 /// NOTE: not fully parsed, still have to figure out how to differentiate
 /// the payload and the 4-byte big-endian integer page number for the
 /// first page of the overflow page list
+/// For now, we will only handle cases without overflow
 #[derive(Debug)]
 #[binrw]
 #[brw(big)]
@@ -69,11 +71,12 @@ pub struct BTreeTableLeafCell {
     pub nb_bytes_key_payload_including_overflow: u64,
     #[br(parse_with = parse_varint)]
     pub integer_key: u64,
-    pub record: Record,
-    #[br(count = nb_bytes_key_payload_including_overflow)]
-    /// initial portion of the payload that does not spill to overflow pages
-    pub payload: Vec<u8>,
-    // REST not parsed
+    pub record_header: RecordHeader,
+    // #[br(count = nb_bytes_key_payload_including_overflow)]
+    // initial portion of the payload that does not spill to overflow pages
+    // we suppose there is no overflow for now
+    // pub payload: Vec<u8>,
+    // REST not parsed - we suppose there is no overflow
 }
 
 /// TODO: actually parse the record to improve the BTreeTableLeafCell
@@ -82,15 +85,15 @@ pub struct BTreeTableLeafCell {
 #[derive(Debug)]
 #[binrw]
 #[brw(big)]
-pub struct Record {
+pub struct RecordHeader {
     #[br(parse_with = parse_record_header)]
-    pub column_type_varints: Vec<RecordType>,
+    pub column_type_varints: Vec<ObjectType>,
 }
 
 #[binrw]
 #[brw(big)]
 #[derive(Debug, Clone)]
-pub enum RecordType {
+pub enum ObjectType {
     NullRecord,
     Int8Record,
     Int16Record,
@@ -106,24 +109,24 @@ pub enum RecordType {
     String(u64),
 }
 
-impl TryFrom<u64> for RecordType {
+impl TryFrom<u64> for ObjectType {
     type Error = binrw::Error;
 
     fn try_from(serial_type: u64) -> Result<Self, Self::Error> {
         Ok(match serial_type {
-            0 => RecordType::NullRecord,
-            1 => RecordType::Int8Record,
-            2 => RecordType::Int16Record,
-            3 => RecordType::Int24Record,
-            4 => RecordType::Int32Record,
-            5 => RecordType::Int48Record,
-            6 => RecordType::Int64Record,
-            7 => RecordType::Float64Record,
-            8 => RecordType::Integer0,
-            9 => RecordType::Integer1,
-            n if n == 10 || n == 11 => RecordType::Reserved,
-            n if n >= 12 && n % 2 == 0 => RecordType::Blob((n - 12) / 2),
-            n if n >= 13 && n % 2 == 1 => RecordType::String((n - 13) / 2),
+            0 => ObjectType::NullRecord,
+            1 => ObjectType::Int8Record,
+            2 => ObjectType::Int16Record,
+            3 => ObjectType::Int24Record,
+            4 => ObjectType::Int32Record,
+            5 => ObjectType::Int48Record,
+            6 => ObjectType::Int64Record,
+            7 => ObjectType::Float64Record,
+            8 => ObjectType::Integer0,
+            9 => ObjectType::Integer1,
+            n if n == 10 || n == 11 => ObjectType::Reserved,
+            n if n >= 12 && n % 2 == 0 => ObjectType::Blob((n - 12) / 2),
+            n if n >= 13 && n % 2 == 1 => ObjectType::String((n - 13) / 2),
             x => {
                 return Err(binrw::Error::Io(std::io::Error::new(
                     std::io::ErrorKind::InvalidData,
@@ -168,17 +171,95 @@ fn parse_varint_with_bytes() -> BinResult<(u64, usize)> {
 }
 
 #[binrw::parser(reader, endian)]
-fn parse_record_header() -> BinResult<Vec<RecordType>> {
-    let (nb_bytes_record_header, header_bytes_read) = parse_varint_with_bytes(reader, endian, ())?;
+fn parse_record_header() -> BinResult<Vec<ObjectType>> {
+    let (size_header, header_bytes_read) = parse_varint_with_bytes(reader, endian, ())?;
 
     let mut records_type = Vec::new();
     let mut total_bytes_read = header_bytes_read as u64;
-    while total_bytes_read < nb_bytes_record_header {
+    while total_bytes_read < size_header {
         let (varint, bytes_read) = parse_varint_with_bytes(reader, endian, ())?;
-        let record_type = RecordType::try_from(varint)?;
+        // dbg!(varint, bytes_read);
+        let record_type = ObjectType::try_from(varint)?;
         records_type.push(record_type);
         total_bytes_read += bytes_read as u64;
     }
+
+    for record_type in &records_type {
+        match record_type {
+            ObjectType::NullRecord => {
+                dbg!("NULL");
+            }
+            ObjectType::Int8Record => {
+                let mut buf = [0u8; 1];
+                reader.read_exact(&mut buf)?;
+                let val = u8::from_be_bytes(buf);
+                dbg!(val);
+            }
+            ObjectType::Int16Record => {
+                let mut buf = [0u8; 2];
+                reader.read_exact(&mut buf)?;
+                let val = u16::from_be_bytes(buf);
+                dbg!(val);
+            }
+            ObjectType::Int24Record => {
+                let mut buf = [0u8; 3];
+                reader.read_exact(&mut buf)?;
+                let val: u32 = (buf[0] as u32) << 16 + (buf[1] as u32) << 8 + buf[2] as u32;
+                dbg!(val);
+            }
+            ObjectType::Int32Record => {
+                let mut buf = [0u8; 4];
+                reader.read_exact(&mut buf)?;
+                let val = u32::from_be_bytes(buf);
+                dbg!(val);
+            }
+            ObjectType::Int48Record => {
+                let mut buf = [0u8; 6];
+                reader.read_exact(&mut buf)?;
+                let val: u64 = (buf[0] as u64)
+                    << 40 + (buf[1] as u64)
+                    << 32 + (buf[2] as u64)
+                    << 24 + (buf[3] as u64)
+                    << 16 + (buf[4] as u64)
+                    << 8 + (buf[5] as u64);
+                dbg!(val);
+            }
+            ObjectType::Int64Record => {
+                let mut buf = [0u8; 8];
+                reader.read_exact(&mut buf)?;
+                let val = u64::from_be_bytes(buf);
+                dbg!(val);
+            }
+            ObjectType::Float64Record => {
+                let mut buf = [0u8; 8];
+                reader.read_exact(&mut buf)?;
+                let val = f64::from_be_bytes(buf);
+                dbg!(val);
+            }
+            ObjectType::Integer0 => {
+                let val = 0;
+                dbg!(val);
+            }
+            ObjectType::Integer1 => {
+                let val = 1;
+                dbg!(val);
+            }
+            ObjectType::Reserved => todo!(),
+            ObjectType::Blob(x) => {
+                let mut buf = vec![0u8; *x as usize];
+                reader.read_exact(&mut buf)?;
+                dbg!("BLOB");
+            }
+            ObjectType::String(x) => {
+                let mut buf = vec![0u8; *x as usize];
+                reader.read_exact(&mut buf)?;
+                let val = String::from_utf8_lossy(&buf);
+                dbg!(val);
+            }
+        }
+    }
+
+    // let columns =
 
     Ok(records_type)
 }
